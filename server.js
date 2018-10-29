db3ebe04a9643d275a28f2ceb45be8923ec123de
Oleg Markoff <mawglih@gmail.com>
2018-10-28 23:54:49 -0400

    initial

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const models = require('./models');
+const expressGraphQL = require('express-graphql');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const passport = require('passport');
+const passportConfig = require('./services/auth');
+const MongoStore = require('connect-mongo')(session);
+const schema = require('./schema/schema');
+
+const app = express();
+const port = 4050;
+const MONGO_URI = 'mongodb://todo-admin:oleg1967@ds143143.mlab.com:43143/todo-app';
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(MONGO_URI);
+mongoose.connection
+  .once('open', () => console.log('Connected to MongoLab instance.'))
+  .on('error', error => console.log('Error connecting to MongoLab:', error));
+
+  // Configures express to use sessions.  This places an encrypted identifier
+// on the users cookie.  When a user makes a request, this middleware examines
+// the cookie and modifies the request object to indicate which user made the request
+// The cookie itself only contains the id of a session; more data about the session
+// is stored inside of MongoDB.
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'aaabbbccc',
+  store: new MongoStore({
+    url: MONGO_URI,
+    autoReconnect: true
+  })
+}));
+// Passport is wired into express as a middleware. When a request comes in,
+// Passport will examine the request's session (as set by the above config) and
+// assign the current user to the 'req.user' object.  See also servces/auth.js
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/graphql', expressGraphQL({
+  schema,
+  graphiql: true
+}));
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+});
